cdb09863288b2f3645e19dc96a4fb84a74253221
Amelia Roberts <arob0017@student.monash.edu>
2020-12-30 14:06:53 +1100

    final touches, badges working

--- generator.js
+++ generator.js
@@ -1,23 +1,35 @@
 function generateReadMe(answers) {
-    return `
-    # Title: ${answers.title}
-    * ${answers.repo}
-    * ${answers.username}
-    
-    ### Contents: ${answers.contents}
-    ### Description ${answers.description}
+  return `# Title: ${answers.title}
 
+  ![License Badge](https://img.shields.io/badge/license-${answers.license}-yellowgreen)  
     
-    #### Installation: 
-    * ${answers.installation}
-    #### Usage: 
-    * ${answers.usage}
-    #### Contributors: 
-    * ${answers.contributors}
-    #### Tests: 
-    * ${answers.tests}
-    #### Questions: 
-    * ${answers.questions}
+  ## Table of Contents: 
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#Contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)  <br> 
+  ## Description 
+  ${answers.description} <br>
+
+  ### Installation 
+  ${answers.installation} <br>
+
+  ### Usage 
+  ${answers.usage} <br>
+  ### License
+  * ${answers.license} <br><br>
+
+  ### Contributing 
+  * ${answers.contributors}
+  ### Tests 
+  * ${answers.tests}
+  ### Questions 
+  Feel free to contact me @ [github.com/${answers.username}](https://www.github.com/${answers.username})<br>
+  If you have further questions please feel free to contact me using this email address:<br>
+  <${answers.questions}>
   
   `;
 }
--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var inquirer = require("inquirer");
 var fs = require('fs');
 const util = require("util");
 
+
 const generator = require("./generator");
 const api = require("./api");
+
+
 
 const writeFileAsync = util.promisify(fs.writeFile);
 // array of questions for user
@@ -18,7 +21,7 @@ function promptUser() {
         },
         {
             type: "input",
-            name: "Repo",
+            name: "repo",
             message: "What is the name of your GitHub repo?",
             default: "README_Generator",
         },
@@ -26,17 +29,12 @@ function promptUser() {
             type: "input",
             name: "title",
             message: "What is your projects title?",
-            default: "ReadMe Generator"
+            default: "ReadMe_Generator"
         },
         {
             type: "input",
             name: "description",
             message: "Please write a description of your project"
-        },
-        {
-            type: "input",
-            name: "contents",
-            message: "Please provide a table of contents"
         },
         {
             type: "input",
@@ -49,9 +47,10 @@ function promptUser() {
             message: "Please provide examples of your application in use?"
         },
         {
-            type: "input",
+            type: "list",
             name: "license",
-            message: "What license and/or licenses were used for this project?"
+            message: "What license was used for this project?",
+            choices: ["Apache", "MIT", "BSD", "GPL", "Mozilla", "LGPL"]
         },
         {
             type: "input",
@@ -61,12 +60,12 @@ function promptUser() {
         {
             type: "input",
             name: "tests",
-            message: "Please provide tests written for your application:?"
+            message: "Please provide tests written for your application:"
         },
         {
             type: "input",
             name: "questions",
-            message: "Please provide a question/s or anything that has not been covered in these data questions?"
+            message: "Please provide your email address:"
         },
 
     ]);
@@ -87,6 +86,30 @@ async function init() {
         const readMe = generator(answers, userInfo);
         await writeFileAsync("index.md", readMe)
         console.log("Successfully wrote to README!")
+        var licence = ""
+
+        switch (answers.licence) {
+            case "Apache":
+                lience = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+                break;
+            case "MIT":
+                licence = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+                break;
+            case "BSD":
+                licence = "[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)"
+                break;
+            case "GPL":
+                licence = "[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+                break;
+            case "Mozilla":
+                licence = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+                break;
+            case "LGPL":
+                licence = "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)"
+                break;
+
+
+        }
     } catch (err) {
         console.log(err);
     }
